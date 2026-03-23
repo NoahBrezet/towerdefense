c57270438b70ba702e7b9575e676ccab2762da43
noah <143404345+NoahBrezet@users.noreply.github.com>
2026-03-23 11:11:00 +0000

    bosst and money towers and more

--- code.py
+++ code.py
@@ -19,9 +19,18 @@ turn = 0
 enemypath = []
 enemyhealth = []
 towerblock = []
+towerdmgbase = []
 towerdmg = []
 towerrange = []
-towerrangetype = [] #di,sq, str
+towerrangetype = [] #di,sq, str, money, boost
+
+class1 = "farm" #farm, castle, church, mage
+class2 = "castle"
+ctower = []
+ctowercost = []
+ctowerdmg = []
+ctowerrange = []
+ctowerrangetype = []
 
 path1 = [2,12,22,32,42,43,44,34,24,14,15,16,17,27,28,38,39,49,59,69,68,78,77,87,86,85,84,74,64,63,62,72,82,92]
 path2 = [2,12,22,23,24,14,15,16,17,27,28,38,39,49,59,69,68,78,77,87,86,85,84,83,82,92]
@@ -147,14 +156,15 @@ def secinput(timeout):
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 def map():
-    string = "1 2 3 4 5 6 7 8 9 10\n"
+    string = "  0 1 2 3 4 5 6 7 8 9"
     count = 1
     with open("map.txt", "w") as f:
         for i in range(10):
+            string = string + "\n" + str(i) + " "
             for j in range(10):
                 string = string + block[count] + " "
                 count += 1
-            string = string + "\n"
+        string = string + "\n"
         f.write(string)
 
 def update():
@@ -166,33 +176,60 @@ def update():
         string = string + "Press 'p' to stop.\nPress '1' to place a farmer's hut for 15 money.\nPress '2' to place a archer tower for 25 money.\n"
         f.write(string)
 
+def boost_update():
+    for i in range(len(towerdmg)):
+        towerdmg[i] = towerdmgbase[i]
+    for i in range(len(towerblock)):
+        if towerrangetype[i] == "boost":
+            if block[towerblock[i]] == "A":
+                hitarea = diamondrange(towerblock[i], towerrange[i])
+                for j in range(len(towerblock)):
+                    if towerblock[j] in hitarea and towerrangetype[j] != "money" and towerrangetype[j] != "boost":
+                        towerdmg[j] = towerdmg[j]+towerdmg[i]
+            if block[towerblock[i]] == "C":
+                for j in range(len(towerblock)):
+                    if towerrangetype[j] != "money":
+                        towerdmg[j] = towerdmg[j]+towerdmg[i]
+
 def place(x,cost,dmg,range,rangetype):
     global money
     if money < cost:
         return
     map()
     with open("map.txt", "a") as f:
-        string = "Name a column from 1 to 10 to place the tower.\n"
+        if x == "C":
+            string = "Name a column from 0 to 9 to place the top left corner of the cathedral.\n"
+        else:
+            string = "Name a column from 0 to 9 to place the tower.\n"
         f.write(string)
     column = ainput()
     map()
     with open("map.txt", "a") as f:
-        string = "Name a row from 2 to 11 to place the tower.\n"
+        if x == "C":
+            string = "Name a row from 0 to 9 to place the top left corner of the cathedral.\n"
+        else:
+            string = "Name a row from 0 to 9 to place the tower.\n"
         f.write(string)
     row = ainput()
     if column.isdigit() and row.isdigit():
-        column = int(column)
-        row = int(row)
-        if 1 <= column <= 10 and 2 <= row <= 11:
-            n = (row-2)*10+column
-            if block[n] == "□":
-                block[n] = x
-                money -= cost
-                bocktowerID[n] = len(bocktowerID) - 1
-                towerblock.append(n)
-                towerdmg.append(dmg)
-                towerrange.append(range)
-                towerrangetype.append(rangetype)
+        n = int(row)*10 + int(column) + 1
+        if x == "C":
+            if block[n] != "□" or block[n+1] != "□" or block[n+10] != "□" or block[n+11] != "□":
+                return
+            block[n+1] = x
+            block[n+10] = x
+            block[n+11] = x
+        elif block[n] != "□":
+            return
+        block[n] = x
+        money -= cost
+        bocktowerID[n] = len(bocktowerID) - 1
+        towerblock.append(n)
+        towerdmgbase.append(dmg)
+        towerdmg.append(dmg)
+        towerrange.append(range)
+        towerrangetype.append(rangetype)
+        boost_update()
 
 def layout(newpath, x):
     global path
@@ -229,6 +266,56 @@ for i in range(10):
 
 layout(path1, x1)
 
+for i in [class1, class2]:
+    if i == "farm":
+        ctower.append("H")
+        ctowercost.append(15)
+        ctowerdmg.append(12)
+        ctowerrange.append(1)
+        ctowerrangetype.append("di")
+        ctower.append("F")
+        ctowercost.append(40)
+        ctowerdmg.append(0)
+        ctowerrange.append(0)
+        ctowerrangetype.append("money")
+        ctower.append("W")
+        ctowercost.append(55)
+        ctowerdmg.append(15)
+        ctowerrange.append(3)
+        ctowerrangetype.append("str")
+    elif i == "castle":
+        ctower.append("T")
+        ctowercost.append(25)
+        ctowerdmg.append(6)
+        ctowerrange.append(2)
+        ctowerrangetype.append("di")
+        ctower.append("A")
+        ctowercost.append(30)
+        ctowerdmg.append(2)
+        ctowerrange.append(2)
+        ctowerrangetype.append("boost")
+        ctower.append("B")
+        ctowercost.append(35)
+        ctowerdmg.append(15)
+        ctowerrange.append(3)
+        ctowerrangetype.append("str")
+    elif i == "church":
+        ctower.append("+")
+        ctowercost.append(10)
+        ctowerdmg.append(6)
+        ctowerrange.append(2)
+        ctowerrangetype.append("str")
+        ctower.append("C")
+        ctowercost.append(75)
+        ctowerdmg.append(0)
+        ctowerrange.append(0)
+        ctowerrangetype.append("boost")
+        ctower.append("Ʌ")
+        ctowercost.append(30)
+        ctowerdmg.append(12) 
+        ctowerrange.append(1)
+        ctowerrangetype.append("sq")
+
 with open("map.txt", "w") as f:
     f.write("Welcome to Tower Defense!\nYou can place a farmer's hut with the '1' key.\nYou can place a archer tower with the '2' key.\nPress 'p' to stop.\nPress any button to continue.")
 
@@ -240,12 +327,27 @@ while True:
     if action == "p":
         exit()
     elif action == "1":
-        place("H", 15, 12, 1, "di")
+        place(ctower[0], ctowercost[0], ctowerdmg[0], ctowerrange[0], ctowerrangetype[0])
     elif action == "2":
-        place("T", 25, 8, 2, "di")
+        place(ctower[3], ctowercost[3], ctowerdmg[3], ctowerrange[3], ctowerrangetype[3])
+    elif action == "3":
+        if round > 3:
+            place(ctower[1], ctowercost[1], ctowerdmg[1], ctowerrange[1], ctowerrangetype[1])
+    elif action == "4":
+        if round > 3:
+            place(ctower[4], ctowercost[4], ctowerdmg[4], ctowerrange[4], ctowerrangetype[4])
+    elif action == "5":
+        if round > 7:
+            place(ctower[2], ctowercost[2], ctowerdmg[2], ctowerrange[2], ctowerrangetype[2])
+    elif action == "6":
+        if round > 7:
+            place(ctower[5], ctowercost[5], ctowerdmg[5], ctowerrange[5], ctowerrangetype[5])
     elif action == "n":
         round += 1
         turn = 0
+        for i in range(len(towerblock)):
+            if towerrangetype[i] == "money":
+                money += towerdmg[i]
         if round == 4:
             layout(path2, x2)
         elif round == 11:
@@ -257,7 +359,7 @@ while True:
             if enemyhealth[i] <= 0:
                 enemypath.pop(i)
                 enemyhealth.pop(i)
-                money += 5
+                money += 2
             elif enemypath[i] < len(path)-1:
                 enemypath[i] += 1
                 block[path[enemypath[i]]] = "▼"
@@ -274,7 +376,9 @@ while True:
             enemyhealth.append(36+round*2)
             block[path[0]] = "▼"
         for i in range(len(towerblock)):
-            if towerrangetype[i] == "di":
+            if towerrangetype[i] == "money"or towerrangetype[i] == "boost":
+                continue
+            elif towerrangetype[i] == "di":
                 hitarea = diamondrange(towerblock[i], towerrange[i])
             elif towerrangetype[i] == "sq":
                 hitarea = squarerange(towerblock[i], towerrange[i])
